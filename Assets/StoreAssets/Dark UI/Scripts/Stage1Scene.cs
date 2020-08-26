44a0b397185b219697885e703693c167babb0c89
dhtpwls0170@naver.com <dhtpwls0170@naver.com>
2020-08-26 13:40:25 +0900

    loadScene

---
+++ Assets/StoreAssets/Dark UI/Scripts/Stage1Scene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage1Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+ public void onClick()
+    {
+        SceneManager.LoadScene("Scene_A");
+    }
+}
