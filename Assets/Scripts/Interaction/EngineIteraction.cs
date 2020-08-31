bacf57992a3b703826ea11ccbf1726fc963c7a37
DESKTOP-2ES5UP7\cheon <cheongAh0926@gmail.com>
2020-08-31 13:10:29 +0900

    [박청아] 게이지바

--- Assets/Scripts/Interaction/EngineIteraction.cs
+++ Assets/Scripts/Interaction/EngineIteraction.cs
@@ -19,9 +19,17 @@ public class EngineIteraction : MonoBehaviour, IObjectInteraction
     private Material matBoby;
     private Material matCap;
 
+    [SerializeField]
+    private GameObject canvas;
+    [SerializeField]
+    private GameObject gaugeBar;
+    private float maxGaugeBarWidth;
     private float gauge;
     [SerializeField]
-    private float requiredTime;
+    private float plusGaugePerSec = 5;
+    private Vector3 gaugeScaleVec;
+
+
 
     void Start()
     {
@@ -32,22 +40,36 @@ public class EngineIteraction : MonoBehaviour, IObjectInteraction
         baseColor_matCap = matCap.color;
 
         gauge = 0f;
+        maxGaugeBarWidth = gaugeBar.transform.localScale.x;
+        gaugeScaleVec = new Vector3(0, 1, 1);
+        gaugeBar.transform.localScale = gaugeScaleVec;
     }
 
     public void Signal()
     {
+        if (gauge >= 100f)
+            return;
+
         matBoby.color = signalColor1;
         matCap.color = signalColor1;
+
+        canvas.SetActive(true);
     }
 
     public void SignalChange()
     {
+        if (gauge >= 100f)
+            return;
+
         matBoby.color = signalColor2;
         matCap.color = signalColor2;
     }
 
     public void SignalChangeBack()
     {
+        if (gauge >= 100f)
+            return;
+
         matBoby.color = signalColor1;
         matCap.color = signalColor1;
     }
@@ -56,25 +78,34 @@ public class EngineIteraction : MonoBehaviour, IObjectInteraction
     {
         matBoby.color = baseColor_matBody;
         matCap.color = baseColor_matCap;
+
+        canvas.SetActive(false);
     }
 
     public void Interact()
     {
-        if (gauge == 0f)
+        if (gauge <= 0f)
         {
             matBoby.SetColor("_EmissionColor", new Color(0, 0, 1));
             matCap.SetColor("_EmissionColor", new Color(0, 0, 1));
         }
 
-        gauge += Time.deltaTime;
+        else if (gauge >= 100f)
+            return;
+
+        gauge += Time.deltaTime * plusGaugePerSec;
+        gaugeScaleVec = new Vector3(gauge * 0.01f, 1, 1);
+        gaugeBar.transform.localScale = gaugeScaleVec;
         //Debug.Log(gauge);
 
-        if (gauge >= requiredTime)
+        if (gauge >= 100f)
         {
-            gauge = requiredTime;
+            gauge = 100f;
 
             matBoby.SetColor("_EmissionColor", new Color(0, 1, 0));
             matCap.SetColor("_EmissionColor", new Color(0, 1, 0));
+
+            SignalEnd();
         }
     }
 }
---
+++ Assets/Scripts/Interaction/UIRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class UIRotate : MonoBehaviour
+{
+    Transform CamTransform;
+
+    void Start()
+    {
+        CamTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        transform.LookAt(transform.position + CamTransform.rotation * Vector3.forward, CamTransform.rotation * Vector3.up);
+    }
+}
