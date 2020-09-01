8a70377ac9fa9a4d2313101fac019caf75a943dc
DESKTOP-2ES5UP7\cheon <cheongAh0926@gmail.com>
2020-09-01 12:44:54 +0900

    [박청아] 변동사항 거의 없음

---
+++ Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+
+public class LivingEntity : MonoBehaviour, IDamageable
+{
+    public float startingHealth = 100.0f;
+    public float health { get; protected set; }
+    public bool dead { get; protected set; }
+    public event Action onDeath;
+
+    protected virtual void OnEnable()
+    {
+        dead = false;
+        health = startingHealth;
+    }
+
+    public virtual void OnDamage(float damage)
+    {
+        health -= damage;
+
+        if (health <= 0 && !dead)
+        {
+            Die();
+        }
+    }
+
+    public virtual void RestoreHealth(float newHealth)
+    {
+        if (dead)
+        {
+            return;
+        }
+
+        health += newHealth;
+    }
+
+    public virtual void Die()   
+    {
+        if (onDeath != null)
+        {
+            onDeath();
+        }
+
+        dead = true;
+    }
+}
