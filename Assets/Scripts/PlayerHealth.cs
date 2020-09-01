cfbcaff6463ace023a734983a68b2d70990c7960
keipro <keiprogramo@gmail.com>
2020-09-01 11:39:40 +0900

    Slender man Attack

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : LivingEntity
+{
+    private Animator playerAnimator;
+
+    private void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+    }
+
+    public override void OnDamage(float damage)
+    {
+        base.OnDamage(damage);
+
+        playerAnimator.SetBool("isInjured", true);
+    }
+
+    public override void Die()
+    {
+        base.Die();
+
+        playerAnimator.SetTrigger("isDie");
+    }
+
+}
