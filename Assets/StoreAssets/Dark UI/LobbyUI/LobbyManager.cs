b034bfc5a21b09bcb941e19064767756c15b30d0
OSJ <dhtpwls0170@naver.com>
2020-09-01 11:13:48 +0900

    networkver1

---
+++ Assets/StoreAssets/Dark UI/LobbyUI/LobbyManager.cs
@@ -0,0 +1,62 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    private string gameVersion = "1";
+
+    public Text connectionInfoText;
+    public Button joinButton;
+    // Start is called before the first frame update
+   private void Start()
+    {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+
+        joinButton.interactable = false;
+        connectionInfoText.text = "마스터 서버(포톤)에 접속 중..";
+    }
+
+    // 마스터 서버 접속 성공 시 실행
+    public override void OnConnectedToMaster()
+    {
+        joinButton.interactable = true;
+        connectionInfoText.text = "온라인 : 마스터 서버(포톤)와 연결됨";
+    }
+
+    // 마스터 서버 접속 실패 시 실행
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        joinButton.interactable = false;
+        connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음\n 접속 재시도 중...";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // 룸 접속 시도
+    public void Connect()
+    {
+        joinButton.interactable = false;
+
+        if(PhotonNetwork.IsConnected)
+        {
+            connectionInfoText.text = "룸에 접속..";
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    // 빈방없고 랜덤룸 참가에 실패경우 실행
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        connectionInfoText.text = "빈 방이 없음, 새로운 방 생성";
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 5 });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        connectionInfoText.text = "참가 성공";
+        PhotonNetwork.LoadLevel("Scene_A");
+    }
+}
