1e1e4f104fc6f183d0254367aa5a58a66ccb8f07
keipro <keiprogramo@gmail.com>
2020-09-01 18:08:18 +0900

    자료 정리 및 업데이트

---
+++ Assets/Scripts/BridgeDoors.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BridgeDoors : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] bridgeDoors;
+    
+    void Start()
+    {
+        EngineIteraction.generatorClear += RandomDoorInactive;
+    }
+
+    private void RandomDoorInactive()
+    {
+        int randNum = Random.Range(0, 4);
+        bridgeDoors[randNum].SetActive(false);
+        Debug.Log("OK");
+    }
+}
